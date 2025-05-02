2925e008ccf7d2aa77ef499a6981c287cbda361f
toheedulhaq082 <01-134221-082@student.bahria.edu.pk>
2025-05-02 20:25:48 +0500

    remove genre

--- movie/context_processors.py
+++ movie/context_processors.py
@@ -4,22 +4,20 @@ def genres_processor(request):
     # List of genres with corresponding TMDb IDs
     genre_data = [
         {'id': 28, 'name': 'Action'},
+        {'id': 53, 'name': 'Thriller'},
+        {'id': 80, 'name': 'Crime'},
+        {'id': 18, 'name': 'Drama'},
+        {'id': 10749, 'name': 'Romance'},
+        {'id': 35, 'name': 'Comedy'},
         {'id': 12, 'name': 'Adventure'},
         {'id': 16, 'name': 'Animation'},
-        {'id': 35, 'name': 'Comedy'},
-        {'id': 80, 'name': 'Crime'},
-        {'id': 99, 'name': 'Documentary'},
-        {'id': 18, 'name': 'Drama'},
         {'id': 10751, 'name': 'Family'},
         {'id': 14, 'name': 'Fantasy'},
         {'id': 36, 'name': 'History'},
         {'id': 27, 'name': 'Horror'},
         {'id': 10402, 'name': 'Music'},
         {'id': 9648, 'name': 'Mystery'},
-        {'id': 10749, 'name': 'Romance'},
-        {'id': 878, 'name': 'Science Fiction'},
-        {'id': 10770, 'name': 'TV Movie'},
-        {'id': 53, 'name': 'Thriller'},
+        {'id': 878, 'name': 'Science-Fiction'},
         {'id': 10752, 'name': 'War'}
     ]
     return {'genres': genre_data}
